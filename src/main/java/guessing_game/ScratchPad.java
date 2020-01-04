4221d5a24c0bcb3e47e4f5db7e234fb10dbf9f39
Columbus Instructor <instructor@wecancodeit.org>
2020-01-04 14:18:25 -0500

    Afternoon workshop progress

---
+++ src/main/java/guessing_game/ScratchPad.java
@@ -0,0 +1,21 @@
+package guessing_game;
+
+public class ScratchPad {
+
+	public static void main(String[] args) {
+		System.out.println(true&&false);
+
+	}
+//	for (int guessCount = 0; guessCount <= 2||userGuess==WINNING_NUMBER; guessCount++) {
+//		while (userGuess == 0) {
+//			System.out.println("Enter a value between 1 & 10 and \nif you guess right you will win.");
+//			userGuess = input.nextInt();
+//		}
+//
+//		if (userGuess != WINNING_NUMBER) {
+//			System.out.println("Guess again:");
+//			userGuess = input.nextInt();
+//		}
+//		
+//	}
+}
