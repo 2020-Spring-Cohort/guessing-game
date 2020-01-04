b4fcdbc59d3305d5d7b28f753e0d7138a46ed041
Columbus Instructor <instructor@wecancodeit.org>
2020-01-04 12:06:36 -0500

    Initial commint for Guessing Game

---
+++ src/main/java/guessing_game/GuessingGame.java
@@ -0,0 +1,12 @@
+package guessing_game;
+
+import java.util.Scanner;
+
+public class GuessingGame {
+
+	public static void main(String[] args) {
+		Scanner input =new Scanner(System.in);
+		
+	}
+
+}
